22bb228860c020ad988bb7058386308ffdb92c62
garrethil <garrethildebrandt@gmail.com>
2023-12-11 14:25:47 -0500

    add current date and time display in the header

--- script.js
+++ script.js
@@ -8,12 +8,6 @@ $(function () {
 
   var timeEl = $('#time'); 
   var saveEl = $('.saveBtn');
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
 
   saveEl.on('click', function() {
     const parentDiv = this.parentNode;
@@ -31,4 +25,9 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+    var displayDateTime = function () {
+      var thisHour = dayjs().format('MMM DD, YYYY [at] hh a');
+      timeEl.append(thisHour);
+    }
+displayDateTime();
 });
