55b689a54b1d9ca68b065772136bdd33c631e9ef
garrethil <garrethildebrandt@gmail.com>
2023-12-11 15:50:19 -0500

    checktime feature added to see what hours are past/present/future

--- script.js
+++ script.js
@@ -8,26 +8,50 @@ $(function () {
 
   var timeEl = $('#time'); 
   var saveEl = $('.saveBtn');
+  var $hours = $('.hour');
+  var currentHour = parseInt(dayjs().format('HH'));
 
   saveEl.on('click', function() {
     const parentDiv = this.parentNode;
     const savedHr = parentDiv.id;
   })
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+
+  var checkTime = function() {
+
+    $hours.each(function() {
+      const hour = parseInt($(this).attr('data-hour'));
+    
+      if (hour < currentHour) {
+        $(this).addClass('past');
+      } else if (hour === currentHour) {
+        $(this).addClass('present');
+      } else {
+        $(this).addClass('future');
+      }
+
+    });
+  }
+  
+
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+
     var displayDateTime = function () {
-      var thisHour = dayjs().format('MMM DD, YYYY [at] hh a');
-      timeEl.append(thisHour);
+      var now = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+      timeEl.text(now);
     }
+
+  
+
+
+
+
+
 displayDateTime();
+setInterval(displayDateTime, 1000);
+checkTime();
 });
